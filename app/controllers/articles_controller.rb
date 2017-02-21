c6d958aa12c86486dd225dc85557d1c76fdcc334
Marissa Ball <marissaball614@gmail.com>
2017-02-21 13:36:57 -0500

    Style article listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		
 		if @article.save
-			flash[:success] = "Article was successfully created"
+			flash[:success] = "Article was successfully created!"
 			redirect_to article_path(@article)
 		else
 			render 'new'
